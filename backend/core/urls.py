644f0d974c842d4dc21ddd533a18d9f52ff06d7d
Tlecoatl <tlecoatl12@gmail.com>
2026-05-31 19:25:50 -0700

    Add registration endpoint2

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,36 +1,30 @@
-from django.contrib.auth.models import User
-from rest_framework import serializers, generics, permissions
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.contrib.auth.models import User
+from rest_framework import serializers, generics, permissions
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['username','password']
+        fields = ['username', 'password']
 
-        def create(self, validated_data):
-            return User.objects.create_user(**validated_data)
-        
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
     permission_classes = [permissions.AllowAny]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # Auth
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/register/', RegisterView.as_view(), name = 'register'),
-
-    # API
+    path('api/register/', RegisterView.as_view(), name='register'),
     path('api/', include('expenses.urls')),
-
-    # Docs
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
